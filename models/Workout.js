99f72c19ff1263c76411e69fb3df231c5dab5edf
Erin Smith <erin.acumen@gmail.com>
2020-08-01 07:10:22 -0700

    workin on it

---
+++ models/Workout.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema ({
+            type: {
+              type: String,
+            required: "Please select the type of workout."
+            },
+            name: {
+            type: String,
+            trim: true,
+            required: "You must name your Exercize"
+          },
+          distance: {
+            type: Number
+          },
+          duration: {
+            type: Number,
+            required: "Please enter the number of minutes you did this activity."
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          }
+        });
+
+  const WorkoutSchema = new Schema({
+          day: Date,
+          totalDuration: Number,
+          exercises: [ExerciseSchema]
+          });
+
+      WorkoutSchema.methods.setTotalDuration = function() {
+          let duration = 0;
+          this.exercises.forEach(exercise => {
+            duration += exercise.duration;
+          });
+          this.totalDuration = duration;
+          console.log(this.totalDuration);
+        }
+
+    const Workout = mongoose.model("Workout", WorkoutSchema);
+
+    module.exports = Workout;
---
+++ routes/api.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+const mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+//get last workout
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .populate("exercises")
+    .then((dbWorkout) => {
+      dbWorkout.forEach((workout) => workout.setTotalDuration());
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//get all Workouts
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+      res.json(dbWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create({ body })
+    .then((newOne) => {
+      res.json(newOne);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+//add new exercises to current workout
+router.put("/api/workouts/:id", (req, res) => {
+  const myId = req.params.id;
+  const data = req.body;
+  db.Workout.findOneAndUpdate(
+    { _id: myId },
+    {
+      $push: { exercises: data },
+      $set: { day: new Date() },
+      $inc: { totalDuration: data.duration },
+    },
+    { new: true }
+  )
+    .then((workout) => {
+      res.json(workout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
