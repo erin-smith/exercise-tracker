ff30fdad617ab536291b7c8ed1b36ff0298f6f4f
Erin Smith <erin.acumen@gmail.com>
2020-11-08 13:32:02 -0800

    deploy

--- seeders/seed.js
+++ seeders/seed.js
@@ -3,7 +3,9 @@ const db = require("../models");
 
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+       useUnifiedTopology: true,
+       useCreateIndex: true,
+       useFindAndModify: false
 });
 
 let workoutSeed = [
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json())
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost/workout", {
